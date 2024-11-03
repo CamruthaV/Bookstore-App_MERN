2996e7f8b510bf1d226c6789603f33ed36e3afe8
CamruthaV <camruthav03@gmail.com>
2024-11-04 00:36:57 +0530

    Modified backend folder

---
+++ backend/index.js
@@ -0,0 +1,44 @@
+
+import express from "express";
+import { PORT, MongoUrl } from "./config.js";
+import mongoose from "mongoose";
+import booksRoute from "./routes/booksRoute.js"
+import cors from "cors";
+
+const app = express();
+
+//Middleware for parsing request body
+app.use(express.json());
+
+// Middleware to handle CORS (Cross-Origin Resource Sharing)
+// Method 1: Using npm package cors and allowing all origins with default of cors
+app.use(cors());
+
+// Method 2: Using npm package cors and allowing only specific origins
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     methods: ['GET','POST','PUT','DELETE'],
+//     allowedHeaders: ["Content-Type"],
+//   })
+// ); 
+
+app.get('/', (request, response) => {
+  console.log(request);
+  return response.status(234).send("Hello from Homepage");
+});
+
+app.use('/books', booksRoute);
+
+mongoose
+  .connect(MongoUrl)
+  .then(() => {
+    console.log("Connected to DB");
+
+    app.listen(PORT, () => {
+      console.log(`App is listening to port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
